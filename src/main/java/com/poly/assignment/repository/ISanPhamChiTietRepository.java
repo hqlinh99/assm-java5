f5895362c8c8fe5a5652bc774d84bfc6cf9d5240
admin <admin@admins-MacBook-Pro.local>
2024-06-05 11:19:40 +0700

    update

--- src/main/java/com/poly/assignment/repository/ISanPhamChiTietRepository.java
+++ src/main/java/com/poly/assignment/repository/ISanPhamChiTietRepository.java
@@ -1,10 +1,10 @@
 package com.poly.assignment.repository;
 
-import com.poly.assignment.entity.HoaDon;
 import com.poly.assignment.entity.SanPhamChiTiet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,4 +13,18 @@ public interface ISanPhamChiTietRepository extends JpaRepository<SanPhamChiTiet,
     Page<SanPhamChiTiet> findAllBySanPhamIdAndTrangThai(Pageable pageable, String pid, boolean parseBoolean);
 
     Page<SanPhamChiTiet> findAllBySanPhamId(Pageable pageable, String pid);
+
+    Page<SanPhamChiTiet> findAllByTrangThai(Pageable pageable, boolean status);
+
+    @Query("""
+            select e
+            from product_details e
+            where e.maSPCT like %:key% or
+                  e.kichThuoc.ten like %:key% or
+                  e.mauSac.ten like %:key% or
+                  e.sanPham.maSP like %:key% or
+                  e.sanPham.ten like %:key%
+            """)
+    List<SanPhamChiTiet> findAllByKey(String key);
+
 }
--- src/main/java/com/poly/assignment/service/SanPhamChiTietService.java
+++ src/main/java/com/poly/assignment/service/SanPhamChiTietService.java
@@ -1,10 +1,16 @@
 package com.poly.assignment.service;
 
+import com.poly.assignment.entity.SanPham;
 import com.poly.assignment.entity.SanPhamChiTiet;
+import com.poly.assignment.repository.IKichThuocRepository;
+import com.poly.assignment.repository.IMauSacRepository;
 import com.poly.assignment.repository.ISanPhamChiTietRepository;
+import com.poly.assignment.repository.ISanPhamRepository;
 import com.poly.assignment.util.PageUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,48 +21,28 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class SanPhamChiTietService {
 
-    List<SanPhamChiTiet> listSanPhamChiTiet = new ArrayList<>();
+    private final MauSacService mauSacService;
 
-    private KichThuocService kichThuocService;
+    private final KichThuocService kichThuocService;
 
-    private MauSacService mauSacService;
+    private final SanPhamService sanPhamService;
 
-    private SanPhamService sanPhamService;
+    private final ISanPhamChiTietRepository sanPhamChiTietRepository;
 
-    @Autowired
-    private ISanPhamChiTietRepository sanPhamChiTietRepository;
+    private final FileUploadService fileUploadService;
 
-    private @Autowired
-    FileUploadService fileUploadService;
-
-    public SanPhamChiTietService(KichThuocService kichThuocService, MauSacService mauSacService, SanPhamService sanPhamService) {
-        this.kichThuocService = kichThuocService;
-        this.mauSacService = mauSacService;
-        this.sanPhamService = sanPhamService;
-
-        listSanPhamChiTiet.add(new SanPhamChiTiet("1", "SPCT01", "Basic", kichThuocService.findById("1"), mauSacService.findById("1"), sanPhamService.findById("1"), 20, 50.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-tra-5.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("2", "SPCT02", "Kẻ sọc", kichThuocService.findById("2"), mauSacService.findById("2"), sanPhamService.findById("2"), 10, 100.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-xnh-4-5f31f3af-196e-474f-80eb-ce0b4617d518.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("3", "SPCT03", "Tay ngắn", kichThuocService.findById("3"), mauSacService.findById("3"), sanPhamService.findById("3"), 15, 150.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-xdm-4.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("4", "SPCT04", "Caro", kichThuocService.findById("4"), mauSacService.findById("4"), sanPhamService.findById("4"), 15, 100.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-xnh-4-5f31f3af-196e-474f-80eb-ce0b4617d518.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("5", "SPCT05", "Caro", kichThuocService.findById("5"), mauSacService.findById("5"), sanPhamService.findById("5"), 20, 50.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-tit-5-71cfb015-53a4-4258-92ca-bc2e2be1bf5e.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("6", "SPCT06", "Kẻ sọc", kichThuocService.findById("6"), mauSacService.findById("6"), sanPhamService.findById("6"), 10, 100.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-den-5-c0028085-1e0a-4909-8a9a-254b104651d7.jpg?v=1690163848063", true));
-        listSanPhamChiTiet.add(new SanPhamChiTiet("7", "SPCT07", "Caro", kichThuocService.findById("7"), mauSacService.findById("7"), sanPhamService.findById("7"), 15, 200.0, "https://bizweb.dktcdn.net/100/438/408/products/smm4073-xdm-4.jpg?v=1690163848063", true));
-    }
-
-    public List<SanPhamChiTiet> findAll(String status) {
-        if (status.equals("true")) return listSanPhamChiTiet.stream()
-                .filter(sp -> sp.getTrangThai())
-                .collect(Collectors.toList());
-        if (status.equals("false")) return listSanPhamChiTiet.stream()
-                .filter(sp -> !sp.getTrangThai())
-                .collect(Collectors.toList());
-        return listSanPhamChiTiet;
+    public Page<SanPhamChiTiet> findAll(String page, String pageSize, String status) {
+        if (status != null && !status.equals("all")) {
+            return sanPhamChiTietRepository.findAllByTrangThai(PageUtils.getPageable(page, pageSize), Boolean.parseBoolean(status));
+        }
+        return sanPhamChiTietRepository.findAll(PageUtils.getPageable(page, pageSize));
     }
 
     public SanPhamChiTiet findById(String id) {
-        return listSanPhamChiTiet.stream().filter(sp -> sp.getId().equals(id)).findFirst().orElse(null);
+        return sanPhamChiTietRepository.findById(id).orElse(null);
     }
 
     public Page<SanPhamChiTiet> findAllSanPhamChiTietBySanPhamId(String page, String pageSize, String status, String pid) {
@@ -66,19 +52,8 @@ public class SanPhamChiTietService {
         return sanPhamChiTietRepository.findAllBySanPhamId(PageUtils.getPageable(page, pageSize), pid);
     }
 
-    public List<SanPhamChiTiet> findByKey(String key) {
-        List<SanPhamChiTiet> result = new ArrayList<>();
-        for (SanPhamChiTiet sanPhamChiTiet : listSanPhamChiTiet) {
-            if (sanPhamChiTiet.getMaSPCT().contains(key) ||
-                    sanPhamChiTiet.getMauSac().getTen().contains(key) ||
-                    sanPhamChiTiet.getKichThuoc().getTen().contains(key) ||
-                    sanPhamChiTiet.getSanPham().getMaSP().contains(key) ||
-                    sanPhamChiTiet.getSanPham().getTen().contains(key)) {
-                result.add(sanPhamChiTiet);
-            }
-        }
-
-        return result;
+    public Page<SanPhamChiTiet> findByKey(String key) {
+        return new PageImpl<>(sanPhamChiTietRepository.findAllByKey(key));
     }
 
     public void create(SanPhamChiTiet sanPhamChiTiet, String pid, MultipartFile file) throws IOException {
@@ -87,26 +62,23 @@ public class SanPhamChiTietService {
         sanPhamChiTiet.setSanPham(sanPhamService.findById(pid));
         sanPhamChiTiet.setMauSac(mauSacService.findById(sanPhamChiTiet.getMauSac().getId()));
         sanPhamChiTiet.setKichThuoc(kichThuocService.findById(sanPhamChiTiet.getKichThuoc().getId()));
-        listSanPhamChiTiet.add(sanPhamChiTiet);
+        sanPhamChiTietRepository.save(sanPhamChiTiet);
     }
 
     public void update(SanPhamChiTiet sanPhamChiTiet, String pid, MultipartFile file) throws IOException {
-        for (int i = 0; i < listSanPhamChiTiet.size(); i++) {
-            if (listSanPhamChiTiet.get(i).getId().equals(sanPhamChiTiet.getId())) {
-                String anh = fileUploadService.uploadFile(file);
-                if (anh != null)
-                    sanPhamChiTiet.setHinhAnh(anh);
-                else sanPhamChiTiet.setHinhAnh(listSanPhamChiTiet.get(i).getHinhAnh());
-                sanPhamChiTiet.setSanPham(sanPhamService.findById(pid));
-                sanPhamChiTiet.setMauSac(mauSacService.findById(sanPhamChiTiet.getMauSac().getId()));
-                sanPhamChiTiet.setKichThuoc(kichThuocService.findById(sanPhamChiTiet.getKichThuoc().getId()));
-                listSanPhamChiTiet.set(i, sanPhamChiTiet);
-            }
+        if (sanPhamChiTiet.getId() != null) {
+            String anh = fileUploadService.uploadFile(file);
+            if (anh != null)
+                sanPhamChiTiet.setHinhAnh(anh);
+            sanPhamChiTiet.setSanPham(sanPhamService.findById(pid));
+            sanPhamChiTiet.setMauSac(mauSacService.findById(sanPhamChiTiet.getMauSac().getId()));
+            sanPhamChiTiet.setKichThuoc(kichThuocService.findById(sanPhamChiTiet.getKichThuoc().getId()));
+            sanPhamChiTietRepository.save(sanPhamChiTiet);
         }
     }
 
     public void delete(String id) {
-        listSanPhamChiTiet.remove(findById(id));
+        sanPhamChiTietRepository.deleteById(id);
     }
 
 }
