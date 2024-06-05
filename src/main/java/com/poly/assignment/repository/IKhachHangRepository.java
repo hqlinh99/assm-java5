d6285fb5f5c95bc306b8ae11ae2482bf96759dfe
admin <admin@admins-MacBook-Pro.local>
2024-06-05 11:07:52 +0700

    update

--- src/main/java/com/poly/assignment/repository/IKhachHangRepository.java
+++ src/main/java/com/poly/assignment/repository/IKhachHangRepository.java
@@ -1,8 +1,18 @@
 package com.poly.assignment.repository;
 
-import com.poly.assignment.entity.HoaDon;
 import com.poly.assignment.entity.KhachHang;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IKhachHangRepository extends JpaRepository<KhachHang, String> {
+
+    Page<KhachHang> findAllByTrangThai(Pageable pageable, boolean status);
+
+    @Query(value = "select * from customers e where e.ma_khach_hang like %:key% or e.ten like %:key% or e.sdt like %:key%", nativeQuery = true)
+    List<KhachHang> findAllByKey(String key);
+
 }
--- src/main/java/com/poly/assignment/service/KhachHangService.java
+++ src/main/java/com/poly/assignment/service/KhachHangService.java
@@ -1,69 +1,46 @@
 package com.poly.assignment.service;
 
 import com.poly.assignment.entity.KhachHang;
+import com.poly.assignment.repository.IKhachHangRepository;
+import com.poly.assignment.util.PageUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 @Service
+@RequiredArgsConstructor
 public class KhachHangService {
 
-    List<KhachHang> listKhachHang = new ArrayList<>();
+    private final IKhachHangRepository khachHangRepository;
 
-    public KhachHangService() {
-        listKhachHang.add(new KhachHang("1", "KH01", "Luu Thi Hai Yen", "0345674567", true));
-        listKhachHang.add(new KhachHang("2", "KH02", "Hoang Quang Linh", "0312341234", true));
-        listKhachHang.add(new KhachHang("3", "KH03", "Do Thi Hong Khanh", "0987654321", true));
-        listKhachHang.add(new KhachHang("4", "KH04", "Luu Thi Hai Yen", "0323452345", true));
-        listKhachHang.add(new KhachHang("5", "KH05", "Nguyen Thi Bich Le", "0956781234", true));
-        listKhachHang.add(new KhachHang("6", "KH06", "Luu Thi Mai Huong", "0956781234", false));
-        listKhachHang.add(new KhachHang("7", "KH07", "Luu Manh Duy", "0956781234", true));
-        listKhachHang.add(new KhachHang("8", "KH08", "Le Thi Thu Hai", "0956781234", false));
-        listKhachHang.add(new KhachHang("9", "KH09", "Nguyen Thi Bich Hang", "0956781234", false));
-        listKhachHang.add(new KhachHang("10", "KH010", "Luu Van Thanh", "0956781234", true));
-    }
-
-    public List<KhachHang> findAll(String status) {
-        if (status.equals("true")) return listKhachHang.stream()
-                .filter(sp -> sp.getTrangThai())
-                .collect(Collectors.toList());
-        if (status.equals("false")) return listKhachHang.stream()
-                .filter(sp -> !sp.getTrangThai())
-                .collect(Collectors.toList());
-        return listKhachHang;
+    public Page<KhachHang> findAll(String page, String pageSize, String status) {
+        if (status != null && !status.equals("all")) {
+            return khachHangRepository.findAllByTrangThai(PageUtils.getPageable(page, pageSize), Boolean.parseBoolean(status));
+        }
+        return khachHangRepository.findAll(PageUtils.getPageable(page, pageSize));
     }
 
     public KhachHang findById(String id) {
-        return listKhachHang.stream().filter(sp -> sp.getId().equals(id)).findFirst().orElse(null);
-
+        return khachHangRepository.findById(id).orElse(null);
     }
 
-    public List<KhachHang> findByKey(String key) {
-        return listKhachHang.stream()
-                .filter(sp -> sp.getMaKH().contains(key)
-                        || sp.getTen().contains(key)
-                        || sp.getSdt().contains(key))
-                .collect(Collectors.toList());
+    public Page<KhachHang> findByKey(String key) {
+        return new PageImpl<>(khachHangRepository.findAllByKey(key));
     }
 
     public void create(KhachHang khachHang) {
-        khachHang.setId(UUID.randomUUID().toString());
-        listKhachHang.add(khachHang);
+        khachHangRepository.save(khachHang);
     }
 
     public void update(KhachHang khachHang) {
-        for (int i = 0; i < listKhachHang.size(); i++) {
-            if (listKhachHang.get(i).getId().equals(khachHang.getId())) {
-                listKhachHang.set(i, khachHang);
-            }
+        if (khachHang.getId() != null) {
+            khachHangRepository.save(khachHang);
         }
     }
 
     public void delete(String id) {
-        listKhachHang.remove(findById(id));
+        khachHangRepository.deleteById(id);
     }
 
 }
